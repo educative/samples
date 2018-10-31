22d2e751fc1976b7a6f9285cc7b2d8c248c8f44d
Athar Mehboob <athar.mehboob@educative.io>
2018-10-31 19:33:30 +0500

    updated env vars name

--- auth0-signin/src/Auth/Auth.js
+++ auth0-signin/src/Auth/Auth.js
@@ -3,8 +3,8 @@ import auth0 from 'auth0-js';
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENTID,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    clientID: process.env.REACT_APP_AUTH0_CLIENTID,
     redirectUri: 'paste educative callback url here',
     responseType: 'token id_token',
     scope: 'openid'
