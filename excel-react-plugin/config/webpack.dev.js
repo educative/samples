50bd365fe7b681339f1e61d40d110e9d7994b509
Athar Mehboob <athar.mehboob@educative.io>
2018-11-14 15:55:13 +0500

    excel react plugin sample

---
+++ excel-react-plugin/config/webpack.dev.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'eval-source-map',
+    devServer: {
+        publicPath: '/',
+        contentBase: path.resolve('dist'),
+        hot: true,
+        compress: true,
+        overlay: {
+            warnings: false,
+            errors: true
+        },
+        port: 3000,
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+});
---
+++ excel-react-plugin/docker/job.md
@@ -0,0 +1,17 @@
+
+
+### Sample Job to run Educative Auth0 Signin Demo
+
+- **Job Name**: BuildExcelReactPlugin
+
+- **Input Filename**: foo
+
+- **Build Script**: Leave this field empty.
+
+- **Run Script**: cp -r /usercode/* /usr/local/educative/samples/excel-react-plugin
+
+- **Run in Live Container**: checked
+
+- **Ports to allow**: 3000
+
+- **Start Script**: npm start --prefix /usr/local/educative/samples/excel-react-plugin
