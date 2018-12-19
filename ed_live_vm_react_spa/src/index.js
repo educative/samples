23c7ecd15dcc0d5e9e2d29aa001606507fdbdc9c
Athar Mehboob <athar.mehboob@educative.io>
2018-12-19 17:05:19 +0500

    alogolia-react-is src for SPA widget

--- ed_live_vm_react_spa/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-require('./style.css');
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
